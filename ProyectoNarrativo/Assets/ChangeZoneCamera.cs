3981598ffa0168cbb5ad99cbf16f480aad331ad1
RixardAra <richardarayapad@gmail.com>
2025-05-28 17:45:12 -0600

    Pseudocódigo trigger dialogos

--- ProyectoNarrativo/Assets/ChangeZoneCamera.cs
+++ ProyectoNarrativo/Assets/ChangeZoneCamera.cs
@@ -6,10 +6,22 @@ public class ChangeZoneCamera : MonoBehaviour
     public float targetSize = 5f;
     public DamageZone damageZone;
     public Transform respawnPoint;
+    public bool isFirstEntry = true;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            if (isFirstEntry)
+            {
+                isFirstEntry = false;
+
+                // Acá llama al dialogo si lo tiene
+                // if HAY DIALOGOS
+                // ENCOLAR DIALOGOS
+
+
+            }
             CameraManager.Instance.MoveTo(targetPosition.position, targetSize);
             if(respawnPoint != null) { 
             damageZone.respawnPoint = respawnPoint;
